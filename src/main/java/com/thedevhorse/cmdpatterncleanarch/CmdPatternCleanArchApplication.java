a70be250fa71a535926611b5f8b019f2c56b244d
hugop <hugopessego@gmail.com>
2024-11-12 20:45:34 +0000

    Initialize Spring Boot project with Maven Wrapper
    Added necessary build configurations, including Maven wrapper scripts, POM dependencies, and project structure. Set up properties files and application entry point for the "cmd-pattern-clean-arch" project.

---
+++ src/main/java/com/thedevhorse/cmdpatterncleanarch/CmdPatternCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.thedevhorse.cmdpatterncleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdPatternCleanArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmdPatternCleanArchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thedevhorse/cmdpatterncleanarch/CmdPatternCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedevhorse.cmdpatterncleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmdPatternCleanArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
